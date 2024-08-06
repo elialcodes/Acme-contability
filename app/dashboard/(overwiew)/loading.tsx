cef545b597146519537995b10e47286440532703
Noelia <abascalcabreran@gmail.com>
2024-08-06 20:40:56 +0200

    database, sql queries and transmision

---
+++ app/dashboard/(overwiew)/loading.tsx
@@ -0,0 +1,6 @@
+//special file of Next to make streaming in the all page while dinamic components are rendered
+
+import DashboardSkeleton from '@/app/ui/skeletons';
+export default function Loading() {
+  return <DashboardSkeleton />;
+}
--- app/dashboard/page.tsx
+++
@@ -1,6 +0,0 @@
-// page.tsx file inside a folder is used to create independent routes.
-// (in this case, the route is: /dashboard)
-
-export default function Page() {
-  return <p>Dashboard Page</p>;
-}
