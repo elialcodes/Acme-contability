28c37f97f67827db29742b05775364efe12728c8
Noelia <abascalcabreran@gmail.com>
2024-08-21 17:36:33 +0200

    not-found file added

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -7,11 +7,22 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+//function from next/navigation Next module to use when a resource doesn´t exist
+//and returning an 404 error in a nice way):
+import { notFound } from 'next/navigation';
 
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
-  //we obtain the constants invoice y customer from the Promises
+  //we obtain the constants invoice y customer from the all the Promises
   const [invoice, customers] = await Promise.all([fetchInvoiceById(id), fetchCustomers()]);
+
+  //if there is no any invoices, we return the function notFound that searchs and renders
+  //the file not-found.tsx, a special Next file to render an interface inside a specific
+  //route segment
+  if (!invoice) {
+    notFound();
+  }
+
   return (
     <main>
       <Breadcrumbs
---
+++ app/dashboard/invoices/errot.tsx
@@ -0,0 +1,27 @@
+'use client'; //this is a client component
+
+import { useEffect } from 'react';
+
+//there will be 2 props: error (it is an instance of Error (a native JavaScript objet)
+//and reset (it is a function to come back to the previous route segment)
+export default function Error({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
+  useEffect(() => {
+    // Optionally log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
+  return (
+    <main className="flex h-full flex-col items-center justify-center">
+      <h2 className="text-center">Something went wrong!</h2>
+      <button
+        className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
+        onClick={
+          // Attempt to recover by trying to re-render the invoices route
+          () => reset()
+        }
+      >
+        Try again
+      </button>
+    </main>
+  );
+}
